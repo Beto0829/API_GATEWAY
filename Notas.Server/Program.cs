6bea241515140aa9c5869e956f0e4a998413227a
Beto0829 <todoslospaises123@gmail.com>
2024-04-02 17:55:19 -0500

    Agregar archivos de proyecto.

---
+++ Notas.Server/Program.cs
@@ -0,0 +1,55 @@
+
+using Microsoft.EntityFrameworkCore;
+using Notas.Server.Models;
+
+namespace Notas.Server
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            //add BD
+            builder.Services.AddDbContext<MiDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            // Configuracion CORS
+            app.UseCors(options =>
+            {
+                options.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            });
+
+            app.MapControllers();
+
+            //MigrateDbContextExtensions.InitializeDatabase(app);
+
+            app.Run();
+        }
+    }
+}
