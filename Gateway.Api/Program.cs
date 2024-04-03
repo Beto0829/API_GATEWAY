ec7b66282c2fff199c9657920f4ac3f57945403f
Beto0829 <todoslospaises123@gmail.com>
2024-04-02 21:24:48 -0500

    Docker-compose(aun con errores)

--- Gateway.Api/Program.cs
+++ Gateway.Api/Program.cs
@@ -1,8 +1,5 @@
 
-using Microsoft.EntityFrameworkCore;
-using Usuarios.Server.Models;
-
-namespace Usuarios.Server
+namespace Gateway.Api
 {
     public class Program
     {
@@ -13,17 +10,12 @@ namespace Usuarios.Server
             // Add services to the container.
 
             builder.Services.AddControllers();
-
-
-            //add BD
-            builder.Services.AddDbContext<MiDbContext>(options =>
-            {
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-            });
-
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddReverseProxy()
+                .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
             var app = builder.Build();
 
@@ -36,19 +28,7 @@ namespace Usuarios.Server
 
             app.UseHttpsRedirection();
 
-            app.UseAuthorization();
-
-            // Configuracion CORS
-            app.UseCors(options =>
-            {
-                options.AllowAnyOrigin()
-                       .AllowAnyMethod()
-                       .AllowAnyHeader();
-            });
-
-            app.MapControllers();
-
-            MigrateDbContextExtensions.InitializeDatabase(app);
+            app.MapReverseProxy();
 
             app.Run();
         }
